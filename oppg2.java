2d9b3e4e9622f086f5adf3e5d9608405444c3a19
Phillip Dyb-Sandnes <phillipdyb@Phillips-MacBook-Air-2.local>
2025-08-19 10:21:26 +0200

    Oppg1 og Oppg2

---
+++ oppg2.java
@@ -0,0 +1,21 @@
+/*
+Oblig Oppgave 2
+ */
+
+public class oppg2 {
+    public static void main(String[] args) {
+        
+        // Definitions
+        double hour = 1.0; // One Hour
+        double minutes = 1.0; // Minutes
+        double seconds = 1.0; // Seconds
+
+        // Math
+        double total = hour * minutes + seconds;
+
+
+        // Print
+        System.out.println(total);
+
+    }
+}
