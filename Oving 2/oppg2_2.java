69892d86d33374671abc5183ef82a0a2906be49d
Phillip Dyb-Sandnes <phillipdyb@Phillips-MacBook-Air-2.local>
2025-08-26 08:55:17 +0200

    ##

--- Oving 2/oppg2_2.java
+++ Oving 2/oppg2_2.java
@@ -5,16 +5,17 @@ Kommentar med informasjon
 public class oppg2_2 {
     public static void main(String[] args) {
         
+        // Definisjoner
         double kjottdeig1 = 35.90;
         double kjottdeig2 = 39.50;
         double gram1 = 450;
         double gram2 = 500;
 
-        // Pris per gram for kjøttdeig 1
-        double prisPerGram1 = gram1 / kjottdeig1;
-        double prisPerGram2 = gram2 / kjottdeig2;
+        // Pris per gram for kjøttdeig
+        double pris1 = gram1 / kjottdeig1;
+        double pris2 = gram2 / kjottdeig2;
 
-        System.out.println(prisPerGram1 + "og " + prisPerGram2);
+        System.out.println(pris1 + "og " + pris2);
 
     }
 }
