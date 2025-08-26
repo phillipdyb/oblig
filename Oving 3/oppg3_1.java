06c8fa5555f3842661ad032cef4a54ac126bbcbe
Phillip Dyb-Sandnes <phillipdyb@Phillips-MacBook-Air-2.local>
2025-08-26 09:14:43 +0200

    ##

--- Oving 3/oppg3_1.java
+++ Oving 3/oppg3_1.java
@@ -5,7 +5,7 @@ Kommentar med informasjon
 public class oppg3_1 {
     public static void main(String[] args) {
         
-        System.out.println(Ting du vil printe);
+        System.out.println();
 
     }
 }
