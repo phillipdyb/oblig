a9567b53114023ca5dd4b8605aba00cb670fea31
Phillip Dyb-Sandnes <phillipdyb@Phillips-MacBook-Air-2.local>
2025-10-22 11:44:48 +0200

    Gjorde klasser public

--- oving8/Person.java
+++ oving8/Person.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class Person {
+public class Person {
 
     private String firstName = firstName;
     private String lastName = lastName;
