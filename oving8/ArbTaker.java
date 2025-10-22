8a0d4c69c750af46c4a21c4a2f747fb2fff31c5a
Phillip Dyb-Sandnes <phillipdyb@Phillips-MacBook-Air-2.local>
2025-10-22 11:40:02 +0200

    Nye filer for klasser

---
+++ oving8/ArbTaker.java
@@ -0,0 +1,54 @@
+
+package oving8;
+
+class ArbTaker {
+
+    int yearJoined = yearJoined;
+    double monthlySalary = monthlySalary;
+    double taxPercentage = taxPercentage;
+
+    // Attributter personalia (datatype Person), arbtakernr
+    // ansettelsesår, månedslønn og skatteprosent.
+
+    // Constructor
+    public ArbTaker(int yearJoined, double monthlySalary, double taxPercentage) {
+        this.yearJoined = yearJoined;
+        this.monthlySalary = monthlySalary;
+        this.taxPercentage = taxPercentage;
+    }
+
+    // Get yearJoined
+    public int getYearJoined() {
+        return yearJoined;
+    }
+
+    // Get monthlySalary
+    public double getMonthlySalary() {
+        return monthlySalary;
+    }
+
+    // Get taxPercentage
+    public double getTaxPercentage() {
+        return taxPercentage;
+    }
+
+    // Get taxKroner
+    public double getTaxKroner() {
+        int taxKroner = (taxPercentage * monthlySalary);
+        return taxKroner;
+    }
+
+    // Set monthlySalary
+    public double setMonthlySalary() {
+        double monthlySalary = setMonthlySalary();
+        return monthlySalary;
+    }
+
+    // Set taxPercentage
+    public double setTaxPercentage() {
+        double taxPercentage = setTaxPercentage();
+        return taxPercentage;
+    }
+
+
+}
---
+++ oving8/Person.java
@@ -0,0 +1,35 @@
+
+package oving8;
+
+class Person {
+
+    private String firstName = firstName;
+    private String lastName = lastName;
+    private int birthYear = birthYear;
+
+    // Attributter fornavn, etternavn og fødselsår
+
+    // Constructor
+    public Person(String firstName, String lastName, int birthYear) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthYear = birthYear;
+    }
+
+    // Get firstName
+    public String getFirstName() {
+        return firstName;
+    }
+
+    // Get lastName
+    public String getLastName() {
+        return lastName;
+    }
+
+    // Get birthYear
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+
+}
